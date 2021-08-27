c13be0faa993f417e1b19806d954962628a0409d
Eric Tuchscherer <eric@tuchscherer.me>
2021-08-27 16:40:36 -0700

    Drawer opening

--- App.tsx
+++ App.tsx
@@ -6,17 +6,31 @@ import PhotosScreen from './screens/photos';
 import BottomTab from './navigation/bottom-tab';
 import MenuScreen from './screens/menu';
 import ProfileVisitsScreen from './screens/profile-visits';
+import Drawer from './navigation/drawer';
+import { TouchableOpacity } from 'react-native';
+
+function tabs() {
+  return (<BottomTab.Navigator>
+    <BottomTab.Screen name="Home" component={HomeScreen} />
+    <BottomTab.Screen name="Classlist" component={ClasslistScreen} />
+    <BottomTab.Screen name="Photos" component={PhotosScreen} />
+    <BottomTab.Screen name="ProfileVisits" component={ProfileVisitsScreen} />
+    <BottomTab.Screen name="Menu" component={MenuScreen} options={({ navigation }) => ({
+      tabBarButton: (props) => (
+        <TouchableOpacity {...props} onPress={() => {
+          navigation.toggleDrawer();
+        }} />
+      )
+    })}/>
+  </BottomTab.Navigator>)
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTab.Navigator>
-        <BottomTab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <BottomTab.Screen name="Classlist" component={ClasslistScreen} />
-        <BottomTab.Screen name="Photos" component={PhotosScreen} />
-        <BottomTab.Screen name="ProfileVisits" component={ProfileVisitsScreen} />
-        <BottomTab.Screen name="Menu" component={MenuScreen} />
-      </BottomTab.Navigator>
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{ drawerPosition: 'right', headerShown: false }}>
+        <Drawer.Screen name="Drawer Menu" component={tabs} options={{ headerShown: false}}/>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../styles/global-stylesheet';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ProfilesScreen from './profile';
-import CarouselScreen from './carousel';
-
-const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Carousels" component={CarouselScreen} />
-      <Stack.Screen name="Profile" component={ProfilesScreen} />
-    </Stack.Navigator>
+    <View style={globalStyles.container}>
+      <Text>Home</Text>
+    </View>
   );
 }
